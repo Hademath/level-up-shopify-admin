7fab0616236e3052ae69cd4a7b4e41a1a77be986
hademath <hademath1992@gmail.com>
2023-11-28 22:28:12 +0100

    add up

--- app.js
+++ app.js
@@ -61,73 +61,120 @@ const steps = document.querySelectorAll(".card");
    });
  });
 
+
+
 class StepIcons {
   constructor(stepIndex) {
     this.stepIndex = stepIndex;
-    this.icons = document.querySelectorAll(`.card:nth-child(${this.stepIndex + 1}) .step svg`);
+    this.icons = document.querySelectorAll(
+      `.card:nth-child(${this.stepIndex + 1}) .step svg`
+    );
     this.completedIcon = this.icons[this.icons.length - 1];
     this.loaderIcons = Array.from(this.icons).slice(2, this.icons.length - 1);
     this.isLoading = false;
+    this._completed = false;
     this.init();
   }
- 
+
+  set completed(isCompleted) {
+    if (this._completed !== isCompleted) {
+      this._completed = isCompleted;
+      const change = isCompleted ? 1 : -1;
+      console.log(change)
+       updateProgressBar(change);
+    
+    }
+  }
+
+  get completed() {
+    return this._completed;
+  }
+
   init() {
-  const firstIcon = this.icons[0];
-  const secondIcon = this.icons[1];
+    const firstIcon = this.icons[0];
+    const secondIcon = this.icons[1];
 
-  firstIcon.addEventListener('mouseenter', () => this.toggleIcons(secondIcon, firstIcon));
-  firstIcon.addEventListener('mouseleave', () => this.toggleIcons(firstIcon, secondIcon));
+    firstIcon.addEventListener("mouseenter", () =>
+      this.toggleIcons(secondIcon, firstIcon)
+    );
+    firstIcon.addEventListener("mouseleave", () =>
+      this.toggleIcons(firstIcon, secondIcon)
+    );
 
-  secondIcon.addEventListener('mouseenter', () => this.toggleIcons(firstIcon, secondIcon));
-  secondIcon.addEventListener('mouseleave', () => this.toggleIcons(secondIcon, firstIcon));
+    secondIcon.addEventListener("mouseenter", () =>
+      this.toggleIcons(firstIcon, secondIcon)
+    );
+    secondIcon.addEventListener("mouseleave", () =>
+      this.toggleIcons(secondIcon, firstIcon)
+    );
 
-  secondIcon.addEventListener('click', () => this.handleIconClick());
-  this.completedIcon.addEventListener('click', () => this.resetIcons());
+    secondIcon.addEventListener("click", () => this.handleIconClick());
+    this.completedIcon.addEventListener("click", () => this.resetIcons());
   }
 
   toggleIcons(iconToShow, iconToHide) {
-    iconToShow.style.display = 'block';
-    iconToHide.style.display = 'none';
+    iconToShow.style.display = "block";
+    iconToHide.style.display = "none";
   }
 
   handleIconClick() {
-    if (!this.isLoading) {
+       if (!this.isLoading) {
       this.isLoading = true;
-      this.completedIcon.classList.remove('rotate-animation');
+      this.completedIcon.classList.remove("rotate-animation");
+
+      const wasCompleted = this.completed; // Store the previous completion status
 
       this.loaderIcons.forEach((icon, index) => {
         setTimeout(() => {
-          icon.style.display = 'block';
-          icon.classList.add('rotate-animation');
+          icon.style.display = "block";
+          icon.classList.add("rotate-animation");
           if (index === this.loaderIcons.length - 1) {
             setTimeout(() => {
-              this.loaderIcons.forEach((icon) => icon.classList.remove('rotate-animation'));
+              this.loaderIcons.forEach((icon) =>
+                icon.classList.remove("rotate-animation")
+              );
               this.isLoading = false;
-              this.completedIcon.style.display = 'block';
-              this.completedIcon.classList.toggle('completed');
+              this.completedIcon.style.display = "block";
+              this.completedIcon.classList.toggle("completed");
+              this.completed = !this.completed; 
+
+              const isNowCompleted = this.completed; 
+              if (wasCompleted !== isNowCompleted) {
+                updateProgressBar(0); 
+              }
             }, 100);
           }
         }, index * 100);
       });
     } else {
-      this.loaderIcons.forEach((icon) => icon.classList.remove('rotate-animation'));
+      this.loaderIcons.forEach((icon) =>
+        icon.classList.remove("rotate-animation")
+      );
       this.isLoading = false;
     }
   }
 
   resetIcons() {
     if (this.isLoading) return;
-
-    this.completedIcon.classList.toggle('completed');
-    this.completedIcon.style.display = 'none';
-    this.icons[0].style.display = 'block';
-    this.icons[1].style.display = 'none';
-    this.icons[2].style.display = 'none';
-    this.icons[3].style.display = 'none';
-    this.icons[4].style.display = 'none';
-    this.icons[5].style.display = 'none';
-    this.icons[6].style.display = 'none';
-    this.icons[7].style.display = 'none';
+   const wasCompleted = this.completed;
+    this.completedIcon.classList.toggle("completed");
+    this.completedIcon.style.display = "none";
+    // this.icons[0].style.display = "block";
+     this.icons.forEach((icon, index) => {
+      if (index === 0) {
+        icon.style.display = "block";
+      } else {
+        icon.style.display = "none";
+      }
+     });
+     const isNowCompleted = this.completed;
+         if (wasCompleted === isNowCompleted) {
+           const increment =  -1; 
+           console.log(increment)
+           updateProgressBar(increment); 
+         }
+    
+    
   }
 }
 
@@ -137,4 +184,31 @@ let step2 = new StepIcons(1);
 let step3 = new StepIcons(2); 
 let step4 = new StepIcons(3); 
 let step5 = new StepIcons(4); 
+
+
+
+function updateProgressBar(increment) {
+  step1.completed ;
+  step2.completed ;
+  step3.completed ;
+  step4.completed ;
+  step5.completed ;
+  // Update the progress bar based on completed steps
+  const completedSteps = [
+    step1.completed,
+    step2.completed,
+    step3.completed,
+    step4.completed,
+    step5.completed, 
+  ];
+
+  const completedCount = completedSteps.filter((step) => step).length + increment ;
+  const progressPercentage = (completedCount / completedSteps.length) * 100;
+
+  const progressBar = document.querySelector(".progress");
+  const progressText = document.getElementById("steppingProgress");
+
+  progressBar.style.width = `${progressPercentage}%`;
+  progressText.textContent = `${completedCount} / ${completedSteps.length} completed`;
+}
 
